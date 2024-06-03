2be4e542df37571d47e54539b4aab7c6a7dc5c0c
toshiyahata <yahata.toshiya@mic-p.com>
2024-06-03 13:37:43 +0900

    0603

--- practice_pandas.py
+++ practice_pandas.py
@@ -1,19 +1,27 @@
 import pandas as pd
 
-series_data = [10,15,20,25]
-ser = pd.Series(series_data)
+df = pd.read_csv("./titanic.csv")
 
-print(ser)
+# print(df[["Age", "Name"]])
+# print(df[0:10])
+# print(df.head(4))
+# print(df.loc[[2],["Age"]])
+# print(df.iloc[5,6])
+# print(df.iat[0,3])
+# print(df.at[4,"Age"])
+# print(df["Age"]>=34)
+# print(df[df["Age"]>=30])
+# print(df[df["Age"].isin([20,30,40])])
+# print(df.sort_values("PassengerId", inplace = True))
+# print(df)
+# df_ = df.dropna(how = "any")
+# df_ = df.fillna(value = 0)
+# df_ = df.isna()
+# print(df_)
+# print(df.groupby("Gender")[["Gender"]].count())
+# print(df.groupby(["Gender", "Survived"])[["Survived"]].count())
+# print(df["Gender"].value_counts())
+# print(df)
 
-dataframe_data = ([
-    [100, "a", True],
-    [200, "b", False],
-    [150, "d", False],
-    [550, "c", True]
-])
-df = pd.DataFrame(dataframe_data)
-
-df.index=["01","02","03","04"]
-df.columns=["A","B","C"]
-
-#print(df["A","B","C"])
+for index, colmn in df.iterrows():
+    print(colmn, df.at[20, "Age"])
